8089a307b1c5afbb211e537ea79b4b5456a84ac5
Krishna Gupta <krishna.gupta3090@gmail.com>
2025-03-29 12:04:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,23 +24,3 @@ class App extends StatelessWidget {
     );
   }
 }
-
-// Dear Hiring Manager,
-//
-// I am excited to apply for the Android Developer position at Capgemini Hong Kong Limited. With over 12 years of experience maintaining and developing software applications, and leading digital transformation initiatives, I am confident in my ability to support Link International's mission of delivering innovative solutions.
-//
-// - I led a team to implement OMS, KYC module, Mobile App/Frontend APIs, helped Lora Technologies to obtain a trading license from SFC.
-//
-// - Migrating legacy systems to cloud-based platforms, enhancing scalability and security.
-//
-// - Collaborating with stakeholders to deliver tailored IT solutions that improved user satisfaction.
-//
-// I am particularly drawn to Link International's commitment to leveraging technology for better solutions. My expertise in software development, Digital Transformation, SDLC, Team Management and strong leadership skills position me to make an immediate impact.
-//
-// Additionally, I would like to highlight that I am Hong Kong permanent resident, I can join immediately, and my current salary is HKD 56,400 per month.
-//
-// I would welcome the opportunity to discuss how I can contribute to Link International. Thank you for considering my application. I look forward to the possibility of speaking with you.
-//
-// Sincerely,
-// Krishna Gupta
-// 9444 4734
